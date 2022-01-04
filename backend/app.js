e1046070ed173f55c1425112236ad82c5c3b2458
Daps <darpanchavhan75@gmail.com>
2022-01-04 23:50:43 +0530

    [feature] separate the post module with different route file.

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const Post = require("./models/post")
+
+const postRoutes = require("./routes/posts");
 
 const app = express()
 
@@ -26,38 +27,11 @@ app.use((req,res,next) => {
     );
     res.setHeader(
         "Access-Control-Allow-Methods",
-        "GET,POST,PATCH,DELETE,OPTIONS"
+        "GET,POST,PATCH,DELETE,OPTIONS,PUT"
     );
     next();
 });
 
-app.post("/api/posts", (req,res,next) => {
-    const post = new Post({
-        title:req.body.title,
-        content:req.body.content
-    });
-    post.save().then((createdPost) => {
-        res.status(201).json({
-            message:"Post added successfully!",
-            postId:createdPost._id
-        })
-    })
-});
-
-app.get("/api/posts", (req,res,next) => {
-    Post.find()
-    .then((documents) => {
-        res.status(200).json({
-            message:"Posts fetched successfully!",
-            posts:documents
-        })
-    });
-});
-
-app.delete("/api/posts/:id", (req,res,next) => {
-    Post.deleteOne({_id:req.params.id}).then(result => {
-        res.status(200).json({message : "Post deleted!"})
-    })
-})
+app.use("/api/posts", postRoutes);
 
 module.exports = app;
