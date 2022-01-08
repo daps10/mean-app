1f46f98561f3725d6767d4fce98d13c98da7e9ce
Daps <darpanchavhan75@gmail.com>
2022-01-09 03:08:35 +0530

    [feature] this component should passed the imagePath every place.

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -63,11 +63,12 @@ export class PostCreateComponent implements OnInit {
                         id:postData._id, 
                         title:postData.title,
                         content:postData.content, 
-                        imagePath:''
+                        imagePath:postData.imagePath
                     }
                     this.form.setValue({
                         title: this.post.title,
-                        content:this.post.content
+                        content:this.post.content,
+                        image:this.post.imagePath
                     })
                 });               
             } else {
@@ -107,19 +108,22 @@ export class PostCreateComponent implements OnInit {
     
             this.isLoading=false;
             // with the help of service
-            this.postService.addPost(this.form.value.title, this.form.value.content, this.form.value.image)
+            this.postService.addPost(
+                this.form.value.title, 
+                this.form.value.content, 
+                this.form.value.image
+            )
                    
         } else {
             this.postService.updatePost(
                 this.postId,
                 this.form.value.title, 
-                this.form.value.content
-                )
-                this.isLoading=false;
+                this.form.value.content,
+                this.form.value.image
+            );
+            this.isLoading=false;
         }
         this.form.reset();
     }
-
-
 
 }
