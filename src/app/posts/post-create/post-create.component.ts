e2162109d4f7802bf0669c2fb54e13ce57aa45b5
Daps <darpanchavhan75@gmail.com>
2022-01-08 18:19:23 +0530

    [feature] created new variable which can hold the imagePreview file and on image change method given support for update imagePreview if we change an image.

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -15,6 +15,7 @@ export class PostCreateComponent implements OnInit {
     enteredTitle = '';
     enteredContent = '';
     isLoading = false;
+    imagePreview:any;
     private mode = 'create';
     private postId: any;
     public post: any;
@@ -69,11 +70,17 @@ export class PostCreateComponent implements OnInit {
     }
 
     onImagePicked(event:Event) {
-        const file = (event.target as HTMLInputElement).files?[0]:"";
+        const file:any = (<HTMLInputElement>event.target).files?.[0];
         this.form.patchValue({image:file});
         this.form.get("image")?.updateValueAndValidity();
-        console.log(file);
-        console.log(this.form);
+        
+        // fileReader add
+        console.log(file)
+        const reader = new FileReader()
+        reader.onload = () => {
+            this.imagePreview = reader.result;
+        }
+        reader.readAsDataURL(file)
     }
 
     onSavePost(){
