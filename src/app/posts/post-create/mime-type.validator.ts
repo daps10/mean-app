053e05a74f336939e0df0d9ccab3535f51ef0cb6
Daps <darpanchavhan75@gmail.com>
2022-01-09 03:07:44 +0530

    [feature] in mimetype has checked the file type is string or object.

--- src/app/posts/post-create/mime-type.validator.ts
+++ src/app/posts/post-create/mime-type.validator.ts
@@ -1,7 +1,11 @@
 import { AbstractControl } from "@angular/forms";
-import { Observable,Observer } from "rxjs";
+import { Observable,Observer,of } from "rxjs";
 
 export const mimeType = (control:AbstractControl): Promise<{[key:string]: any}> | Observable<{[key:string] : any}> => {
+    if(typeof control.value === 'string'){
+        return of({})
+    }
+    
     const file = control.value as File;
     const fileReader:any = new FileReader();
 
