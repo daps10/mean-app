49ee73ee3232c0f0353e2bc470aeb97abbfc22f0
Daps <darpanchavhan75@gmail.com>
2022-01-16 01:26:49 +0530

    [feature] in user routes file added bcrypt hash which can handle the password hashing and store that in the user schema.

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,31 @@
 const express = require("express");
-const Post = require("../models/post")
+const bcrypt = require("bcrypt");
+
+const User = require("../models/user")
 
 const router = express.Router()
 
 router.post("/signup", (req,res,next) => {
-    
+    bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            const user = new User({
+                name:req.body.name, 
+                email:req.body.email, 
+                password:hash
+            });
+            user.save()
+                .then(result => {
+                    res.status(201).json({
+                        message:"User created!",
+                        result:result
+                    });
+                })
+                .catch(err => {
+                    res.status(500).json({
+                        error:err
+                    })
+                }) 
+        });
 })
 
 
