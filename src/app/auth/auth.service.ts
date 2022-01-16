5fb94f90522dc8dd23da6efad1ed8eaf2ac582e8
Daps <darpanchavhan75@gmail.com>
2022-01-16 18:07:59 +0530

    [feature] created login method to call login api.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -10,11 +10,23 @@ export class AuthService {
  
     createUser (name: string, email:string, password:string ) {
         const authData:AuthData = {
-            email:email,
             name:name,
+            email:email,
             password:password
         }
         this.http.post("http://localhost:3000/api/user/signup", authData)
+            .subscribe(response => {
+                console.log(response)
+            })
+    }
+
+    login ( email:string, password:string ) {
+        const authData:AuthData = {
+            email:email,
+            password:password,
+            name:""
+        }
+        this.http.post("http://localhost:3000/api/user/login", authData)
             .subscribe(response => {
                 console.log(response)
             })
