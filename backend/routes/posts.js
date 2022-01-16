f55de71ad7638423fb9185541038b35244c0a80e
Daps <darpanchavhan75@gmail.com>
2022-01-16 18:05:44 +0530

    [feature] attached middleware to every restricted requests.

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,5 +1,7 @@
 const express = require("express");
+
 const Post = require("../models/post")
+const checkAuth = require("../middleware/check-auth")
 const multer = require('multer');
 
 const router = express.Router()
@@ -26,64 +28,74 @@ const storage = multer.diskStorage({
     }
 });
 
-router.post("", multer({storage : storage}).single("image"), (req,res,next) => {
-    const url = req.protocol + '://' + req.get("host");
-    const post = new Post({
-        title:req.body.title,
-        content:req.body.content,
-        imagePath: url + "/images/" + req.file.filename
-    });
-    post.save().then((createdPost) => {
-        res.status(201).json({
-            message:"Post added successfully!",
-            post : {
-                ...createdPost,
-                id:createdPost._id,
-            }
-        })
-    })
-});
-
-router.put("/:id", multer({storage : storage}).single("image"), (req,res,next) => {
-    let imagePath = req.body.imagePath;
-    if(req.file) {
+router.post(
+    "",
+    checkAuth, 
+    multer({storage : storage}).single("image"), 
+    (req,res,next) => {
         const url = req.protocol + '://' + req.get("host");
-        imagePath = url + "/images/" + req.file.filename
-    }
-    const post = new Post({
-        _id:req.body.id,
-        title:req.body.title,
-        content:req.body.content,
-        imagePath: imagePath 
-    });
-    console.log(post)
-    Post.updateOne({_id:req.params.id}, post).then((result) => {
-        res.status(201).json({
-            message:"Post updated successfully!"
+        const post = new Post({
+            title:req.body.title,
+            content:req.body.content,
+            imagePath: url + "/images/" + req.file.filename
+        });
+        post.save().then((createdPost) => {
+            res.status(201).json({
+                message:"Post added successfully!",
+                post : {
+                    ...createdPost,
+                    id:createdPost._id,
+                }
+            })
         })
-    })
 });
 
-router.get("", (req,res,next) => {
-    const pageSize = +req.query.pageSize;
-    const currentPage = +req.query.page;
-    const postQuery= Post.find();
-    let fetchedPosts;
-    if(pageSize && currentPage) {
-        postQuery
-            .skip(pageSize * (currentPage - 1))
-            .limit(pageSize)
-    }
-    postQuery.then((documents) => {
-        fetchedPosts=documents;
-        return Post.count();
-    }).then(count => {
-        res.status(200).json({
-            message:"Posts fetched successfully!",
-            posts:fetchedPosts,
-            maxPosts:count
+router.put(
+    "/:id",
+    checkAuth, 
+    multer({storage : storage}).single("image"), 
+    (req,res,next) => {
+        let imagePath = req.body.imagePath;
+        if(req.file) {
+            const url = req.protocol + '://' + req.get("host");
+            imagePath = url + "/images/" + req.file.filename
+        }
+        const post = new Post({
+            _id:req.body.id,
+            title:req.body.title,
+            content:req.body.content,
+            imagePath: imagePath 
+        });
+        console.log(post)
+        Post.updateOne({_id:req.params.id}, post).then((result) => {
+            res.status(201).json({
+                message:"Post updated successfully!"
+            })
         })
-    });
+});
+
+router.get(
+    "", 
+    (req,res,next) => {
+        const pageSize = +req.query.pageSize;
+        const currentPage = +req.query.page;
+        const postQuery= Post.find();
+        let fetchedPosts;
+        if(pageSize && currentPage) {
+            postQuery
+                .skip(pageSize * (currentPage - 1))
+                .limit(pageSize)
+        }
+        postQuery.then((documents) => {
+            fetchedPosts=documents;
+            return Post.count();
+        }).then(count => {
+            res.status(200).json({
+                message:"Posts fetched successfully!",
+                posts:fetchedPosts,
+                maxPosts:count
+            })
+        });
 });
 
 router.get("/:id", (req,res,next) => {
@@ -99,7 +111,10 @@ router.get("/:id", (req,res,next) => {
     });
 });
 
-router.delete("/:id", (req,res,next) => {
+router.delete(
+    "/:id", 
+    checkAuth,
+    (req,res,next) => {
     Post.deleteOne({_id:req.params.id}).then(result => {
         res.status(200).json({message : "Post deleted!"})
     })
