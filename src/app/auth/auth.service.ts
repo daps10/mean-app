517d6f8b9fc678523850469b092dcc3af4f19cfc
Daps <darpanchavhan75@gmail.com>
2022-02-19 17:17:37 +0530

    Redirecting users
    - Redirecting users after logging in and logging out to the home page.
    - Must have visible spinner when we login and signup actions performed.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Subject } from "rxjs";
+import { Router } from "@angular/router";
 import { HttpClient } from "@angular/common/http";
 import { AuthData } from './auth-data.model';
 
@@ -11,7 +12,7 @@ export class AuthService {
     private token: any;
     private authStatusListener= new Subject<boolean>();
   
-    constructor( private http:HttpClient ) {}
+    constructor( private http:HttpClient, private router: Router ) {}
     getToken () {
         return this.token;
     }
@@ -49,6 +50,7 @@ export class AuthService {
                 if(token) {
                     this.isAuthenticated=true;
                     this.authStatusListener.next(true);
+                    this.router.navigate(['/']);
                 }
             })
     }
@@ -57,5 +59,7 @@ export class AuthService {
         this.token= null;
         this.isAuthenticated= false;
         this.authStatusListener.next(false);
+        
+        this.router.navigate(['/']);
     }
 }
