48428bab43f8090577368cbb3f73d6715abb5124
Daps <darpanchavhan75@gmail.com>
2022-02-20 02:27:31 +0530

    Saving the token in local storage.
    - In auth service created some private methods which can save, clear and fetch the the local storage data.
    - created autoAuthUser method to check user is currently active or not.
    - call that auth service in app.component.ts file which is first loaded everytime.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -51,15 +51,32 @@ export class AuthService {
                 this.token=token;
                 if(token) {
                     const expiresInDuration= response.expiresIn;
-                    this.tokenTimer = setTimeout(() => {
-                        this.logout();
-                    }, expiresInDuration*1000)
-
+                    this.setAuthTimer(expiresInDuration);
+     
                     this.isAuthenticated=true;
                     this.authStatusListener.next(true);
+                    const now= new Date();
+                    const expirationDate= new Date(now.getTime() + expiresInDuration * 1000);
+                    this.saveAuthData(token, expirationDate)
                     this.router.navigate(['/']);
                 }
             })
+    }
+
+    autoAuthUser () {
+        const authInformation= this.getAuthData();
+        if(!authInformation){
+            return;
+        }
+        
+        const now= new Date();
+        const expiresIn= authInformation?.expirationDate.getTime() - now.getTime();
+        if(expiresIn > 0){
+            this.token = authInformation?.token;
+            this.isAuthenticated= true;
+            this.setAuthTimer(expiresIn / 1000);
+            this.authStatusListener.next(true);
+        }
     }
 
     logout () {
@@ -67,6 +84,39 @@ export class AuthService {
         this.isAuthenticated= false;
         this.authStatusListener.next(false);
         clearTimeout(this.tokenTimer);       
+        this.clearAuthData();
         this.router.navigate(['/']);
+    }
+
+    private setAuthTimer (duration: number) {
+        console.log("Setting timer ::", duration)
+        this.tokenTimer = setTimeout(() => {
+            this.logout();
+        }, duration*1000)
+    }
+
+    private saveAuthData(token: string, expirationDate:Date) {
+        // store items in local storage.
+        localStorage.setItem('token', token);
+        localStorage.setItem('expirationDate', expirationDate.toISOString());
+    }
+
+    private clearAuthData() {
+        // remove items from local storage
+        localStorage.removeItem('token');
+        localStorage.removeItem('expirationDate');
+    }
+
+    private getAuthData() {
+        const token= localStorage.getItem('token');
+        const expirationDate= localStorage.getItem('expirationDate');
+        
+        if(!token || !expirationDate){
+            return;
+        }
+        return {
+            token:token, 
+            expirationDate: new Date(expirationDate)
+        }
     }
 }
